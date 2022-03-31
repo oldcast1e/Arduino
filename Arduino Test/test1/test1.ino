0364dc0d77d6cf28b171a9f970a48c35f5108092
oldcast1e <kylhs0207@naver.com>
2022-03-31 14:16:05 +0900

    +

--- Arduino Test/test1/test1.ino
+++ Arduino Test/test1/test1.ino
@@ -1,15 +1,9 @@
-int rst = A0;
 void setup() {
   // put your setup code here, to run once:
-    pinMode(rst,INPUT);
-    Serial.begin(9600);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int val= analogRead(rst);
-  Serial.println(val);
-  delay(100);
 
 }
