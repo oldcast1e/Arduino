bbc44611ff90a4239814490fa2daf7dc8a546783
oldcast1e <kylhs0207@naver.com>
2022-04-04 23:57:13 +0900

    -=

--- Arduino Test/testsource1/testsource1.ino
+++ Arduino Test/testsource1/testsource1.ino
@@ -2,13 +2,15 @@ int LED = 13;
 void setup() {
   // put your setup code here, to run once:
   pinMode(13, OUTPUT);
+  Serial.begin(9600);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED, HIGH);
+  // digitalWrite(LED, HIGH);
+  Serial.println("Hello World!");
   delay(500);
-  digitalWrite(LED, LOW);
+  // digitalWrite(LED, LOW);
 
 }
