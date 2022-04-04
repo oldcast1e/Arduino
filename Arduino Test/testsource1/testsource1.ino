31a98825dfe1ab38e25fa5cc5645ad60327551a2
Lee Heonseong <80825434+oldcast1e@users.noreply.github.com>
2022-04-05 02:10:47 +0900

    Update testsource1.ino

--- Arduino Test/testsource1/testsource1.ino
+++ Arduino Test/testsource1/testsource1.ino
@@ -1,20 +1,21 @@
 void setup() {
   // pinMode(13, OUTPUT);
   Serial.begin(9600);
-  Serial.print("#11P1500T1000\r\n");
+//  Serial.print("#13P1200T1000\r\n");
+  move();move();
 
 }
 
 void move(){
-
-   Serial.println("#11P1200T3000\r\n");
+//
+   Serial.print("#13P1000T500\r\n");
    delay(1000);
-   Serial.print("#11P1500T3000\r\n");
+   Serial.print("#13P1500T500\r\n");
    delay(1000);
-   Serial.println("#11P1800T3000\r\n");
-   delay(1000);
-   Serial.print("#11P1500T3000\r\n");
-   delay(1000);
+//   Serial.print("#11P1800T1000\r\n");
+//   delay(1000);
+//   Serial.print("#11P1500T1000\r\n");
+//   delay(1000);
 }
 
 void loop() {
