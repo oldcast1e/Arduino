04953667427a47a81fc09ea74740de4877b4574d
oldcast1e <kylhs0207@naver.com>
2022-04-05 21:20:27 +0900

    Update testsource2.ino

--- Arduino Test/testsource2/testsource2.ino
+++ Arduino Test/testsource2/testsource2.ino
@@ -1,9 +1,15 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+/**
+블루투스 통신 테스트 코드
+ */
+#include <SoftwareSerial.h>
+SoftwareSerial btSerial = SoftwareSerial(2,3);
 
-int main() {
+void setup(){
+  Serial.begin(9600);
+  btSerial.begin(9600);
+}
 
-    
-    
-}	
+void loop(){
+    if(Serial.available()) btSerial.write(Serial.read());
+    if(btSerial.available()) Serial.write(btSerial.read());
+}
