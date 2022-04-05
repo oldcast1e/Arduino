ab70281cdea32965c076d80c91ef549594be8a25
Lee Heonseong <80825434+oldcast1e@users.noreply.github.com>
2022-04-05 22:13:12 +0900

    Update testsource1.ino

--- Arduino Test/testsource1/testsource1.ino
+++ Arduino Test/testsource1/testsource1.ino
@@ -1,14 +1,6 @@
 /**
   모터 제어 테스트 코드
 */
-void setup() {
-  // pinMode(13, OUTPUT);
-  Serial.begin(9600);
-  armup();
-  delay(3000);
-  armdown();
-}
-
 void armup() {
   Serial.print("#9P2500#13P500\r\n");
 }
@@ -20,6 +12,18 @@ void formset(){
   Serial.print("#1P1500#2P1500#3P1500#4P1500#5P1500#9P1500#10P1500#11P1500#12P1500#13P1500#14P1500#16P1500#17P1500#18P1500#19P1500#20P1500#\r\n");
 
 }
+
+void setup() {
+  // pinMode(13, OUTPUT);
+  Serial.begin(9600);
+  formset();
+  
+  armup();
+  delay(3000);
+  armdown();
+}
+
+
 
 void move() {
   //
