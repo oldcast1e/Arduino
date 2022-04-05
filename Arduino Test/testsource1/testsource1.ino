88aa0bcc82c3be07ca3a133730397b4eedfe5a77
oldcast1e <kylhs0207@naver.com>
2022-04-05 21:53:58 +0900

    Update testsource1.ino

--- Arduino Test/testsource1/testsource1.ino
+++ Arduino Test/testsource1/testsource1.ino
@@ -4,21 +4,21 @@
 void setup() {
   // pinMode(13, OUTPUT);
   Serial.begin(9600);
-//  Serial.print("#13P1200T1000\r\n");
-  move();move();
+  armup();
+}
+
+void armup(){
+  Serial.print("#9P2500#13P500\r\n");
 
 }
 
 void move(){
 //
-   Serial.print("#13P1000T500\r\n");
-   delay(1000);
-   Serial.print("#13P1500T500\r\n");
-   delay(1000);
-//   Serial.print("#11P1800T1000\r\n");
-//   delay(1000);
-//   Serial.print("#11P1500T1000\r\n");
-//   delay(1000);
+  Serial.print("#13P1000T500\r\n");
+  delay(1000);
+  Serial.print("#13P1500T500\r\n");
+  delay(1000);
+
 }
 
 void loop() {
