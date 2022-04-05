39ce2756e2755694175f31fcca538e2f88fd0eb0
Lee Heonseong <80825434+oldcast1e@users.noreply.github.com>
2022-04-05 21:59:16 +0900

    Update testsource1.ino

--- Arduino Test/testsource1/testsource1.ino
+++ Arduino Test/testsource1/testsource1.ino
@@ -1,22 +1,24 @@
 /**
-모터 제어 테스트 코드
- */
+  모터 제어 테스트 코드
+*/
 void setup() {
   // pinMode(13, OUTPUT);
   Serial.begin(9600);
   armup();
+  delay(3000);
+  armdown();
 }
 
-void armup(){
+void armup() {
   Serial.print("#9P2500#13P500\r\n");
 }
 
-void armdown(){
+void armdown() {
   Serial.print("#9P1500#13P1500\r\n");
 }
 
-void move(){
-//
+void move() {
+  //
   Serial.print("#13P1000T500\r\n");
   delay(1000);
   Serial.print("#13P1500T500\r\n");
